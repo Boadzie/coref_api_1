f9f2f328d52489c63176fd7aba6719aa3655da77
User 1 <user1@4th-ir.com>
2021-10-15 16:49:28 +0200

    Add test to api
    Test the post endpoint using httpx and pytest
    
    Add instructions to suppress deprecation error in pytest(pytest.ini)

--- tests/test_coref_api.py
+++ tests/test_coref_api.py
@@ -1,5 +1,20 @@
-from coref_api import __version__
+import coref_api.main as main
+import pytest
+from httpx import AsyncClient
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.fixture
+async def async_app_client():
+    async with AsyncClient(app=main.app, base_url="http://test") as client:
+        yield client
+        
+
+@pytest.mark.asyncio
+async def test_app(async_app_client):
+    payload = {"text": "My sister has a dog. She loves him."}
+
+    response = await async_app_client.post("/coref", json=payload)
+
+
+    assert response.status_code == 200, response.text
+    # assert response.json() == payload
